2da48076d1c93dacbf990a980687102fd4f2f630
urbain4 <armandduvalurbain@hotmail.com>
2019-01-04 14:49:11 +0100

    exo corrigé

--- exo_17.rb
+++ exo_17.rb
@@ -4,7 +4,7 @@ birth_date.to_i.times do |i|
 a = birth_date.to_i
 
 if "#{i + 1}" != "#{a - i - 1}"
-	puts "il y a #{i + 1} ans tu avais #{a - i - 1} ans"
+	puts "il y a #{a - i - 1} ans tu avais #{i + 1} ans"
 
 else "#{i + 1}" == "#{a - i - 1}"
 		puts "il y a #{i + 1} ans, tu avais la moitié de l'age que tu as aujourd'hui !"
