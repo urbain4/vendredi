b15321632484fad3fdfd20ce2c896aae83d770b0
urbain4 <armandduvalurbain@hotmail.com>
2019-01-04 03:29:26 +0100

    le 12 c'est fait

--- exo_11.rb
+++ exo_11.rb
@@ -1,6 +1,6 @@
 puts "qu'elle age as tu?"
 user_age = gets.chomp
 user_age_multiplie = user_age.to_i
-user_age_multiplie.times do
+user_age_multiplie.times do #https://www.ruby-lang.org/fr/documentation/quickstart/4/
 puts "Salut, ça farte ?"
 end
---
+++ exo_12.rb
@@ -0,0 +1,7 @@
+puts "Donnes moi un nombre, sinon ça va barder !"
+nombre = gets.chomp
+nombre = nombre.to_i
+start = 0
+nombre.times do
+  puts start +=1
+end
