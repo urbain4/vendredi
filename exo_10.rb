75b358ceb6e80045cc548719dbc283d0902f9036
urbain4 <armandduvalurbain@hotmail.com>
2019-01-04 02:01:06 +0100

    exo 11 ptet bon

--- exo_10.rb
+++ exo_10.rb
@@ -1,3 +1,4 @@
 puts "Si ce n'est pas indiscret, en quelle année es tu naît?"
 user_age = gets.chomp
+user_age = user_age.to_i
 puts "Du coup tu avais #{2017 - user_age} ans en 2017"
---
+++ exo_11.rb
@@ -0,0 +1,6 @@
+puts "qu'elle age as tu?"
+user_age = gets.chomp
+user_age = user_age.to_i
+user_age.times do
+puts "Salut, ça farte ?"
+end
